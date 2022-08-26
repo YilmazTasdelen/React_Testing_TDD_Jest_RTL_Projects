0f77f456d416a8f28be38c0e2cad404e26e55d90
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-26 18:43:59 +0300

    .

--- sundaes-on-demand/src/pages/entry/tests/Option.test.js
+++ sundaes-on-demand/src/pages/entry/tests/Option.test.js
@@ -19,4 +19,24 @@ test('display image for each scoop option form server', async () => {
     const altText = scoopImages.map((element) => element.alt);
     expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop'])
 
-})
+});
+
+
+test('display image for each toppings option form server', async () => {
+    render(<Option optionType="toppings" />);
+
+    //find images
+    //we use await findallby instead of screen.getAllByRole because images came from asyncapi call 
+    const toppingImages = await screen.findAllByRole('img',
+        {
+            name: /toppping$/i // $ is regular expration that string end with scoop
+        });
+
+    // our mock service return 3 option 
+    expect(toppingImages).toHaveLength(3);
+
+    //confirm alt text of images
+    const altText = toppingImages.map((element) => element.alt);
+    expect(altText).toEqual(['Cherries tooping', 'M&Ms tooping', 'Hot fudge tooping'])
+
+});
