90bc55b69bc7f765d2bf56b056bf29fecba36f76
Yilmaz <yilmaztasdelen1994@gmail.com>
2022-08-26 19:06:02 +0300

    .

--- sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.js
+++ sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.js
@@ -1,6 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import OrderEntry from '../OrderEntry';
+//we  need these two for override some mock methods. we override them for error schenarios.
+import { rest } from 'msw';
+import { server } from '../../../mocks/server';
 
-test('handles eeror for scoops and topping routes', () => {
+
+test('handles eeror for scoops and topping routes', async () => {
+    //ovver ride mock services for error 
+    server.resetHandlers(
+        rest.get('http://localhost:3030/scoops', (req, res, ctx) =>
+            res(ctx.status(500))
+        ),
+        rest.get('http://localhost:3030/toppings', (req, res, ctx) =>
+            res(ctx.status(500))
+        ),
+    );
+
+    // then render the component and test
+    render(<OrderEntry />);
+
+    const alerts = await screen.findAllByRole('alert', {
+        name: 'An unexpected error ocurred. Please try again later.'
+    });
+
+    expect(alerts).toHaveLength(2);
 
 });
